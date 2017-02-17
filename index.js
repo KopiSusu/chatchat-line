5cf3818350a9b7d8b711cc58c891cfb0dd3fdf99
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-16 22:20:37 -0500

    grrrr

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({ extended: false, limit: 2 * 1024 * 1024 }));
 app.use(bodyParser.json({ limit: 2 * 1024 * 1024 }));
 
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    // if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    //     res.send(req.query['hub.challenge'])
+    // }
+    console.log('inside webhook')
+    
+    res.send(200)
+})
+
 app.post('/', function (req, res) {
   console.log('req.body.result :', req.body.result);
 
