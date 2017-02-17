e592903f14afe149f606cfa010942ee6c1fb38d2
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-17 01:26:19 -0500

    more trying

--- index.js
+++ index.js
@@ -1,89 +1,45 @@
-var _ = require('lodash');
-var bodyParser = require('body-parser');
-var express = require('express');
-var request = require('superagent');
-var LineBot = require('line-bot-sdk');
+'use strict'
+const line = require('node-line-bot-api')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-var client = LineBot.client({
-    channelID: '1501651709',
-    channelSecret: '239ce8176ae2ca2edc3939bcf26241ec',
-    channelToken: 'ToDFVvCpNFbfjtxzR2CDLNTt3aSWAhLN2/9jf03d0VGJSqh4DMSJd+fRm4ZH+TfNmzbWt6VCAosZqxsTmvmbIFLh7nQPLztM7/YIIryklIwG65ds9X11voXd8uPXqjabkrgCZYXnzo3dJNwJQwopIQdB04t89/1O/w1cDnyilFU='
-});
+// need raw buffer for signature validation
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
+  }
+}))
 
-var app = express();
+// init with auth
+line.init({
+  accessToken: 'ToDFVvCpNFbfjtxzR2CDLNTt3aSWAhLN2/9jf03d0VGJSqh4DMSJd+fRm4ZH+TfNmzbWt6VCAosZqxsTmvmbIFLh7nQPLztM7/YIIryklIwG65ds9X11voXd8uPXqjabkrgCZYXnzo3dJNwJQwopIQdB04t89/1O/w1cDnyilFU=',
+  // (Optional) for webhook signature validation
+  channelSecret: '239ce8176ae2ca2edc3939bcf26241ec'
+})
 
-app.set('port', (process.env.PORT || 5000));
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    console.log(event.message.text)
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
 
-app.use(bodyParser.urlencoded({ extended: false, limit: 2 * 1024 * 1024 }));
-app.use(bodyParser.json({ limit: 2 * 1024 * 1024 }));
+  Promise
+    .all(promises)
+    .then(() => res.sendStatus({success: true}))
+})
 
-// for line verification
-app.post('/', function (req, res) {
-    console.log('req.body :', req.body)
-    console.log('<-------- Here! ---------->');
-
-    const receives = client.createReceivesFromJSON(req.body);
-    _.each(receives, function(receive){
-        
-        if(receive.isMessage()){
-
-            if(receive.isText()){
-
-                if(receive.getText()==='me'){
-                    client.getUserProfile(receive.getFromMid())
-                        .then(function onResult(res){
-                            if(res.status === 200){
-                                var contacts = res.body.contacts;
-                                if(contacts.length > 0){
-                                    client.sendText(receive.getFromMid(), 'Hi!, you\'re ' + contacts[0].displayName);
-                                }           
-                            }
-                        }, function onError(err){
-                          console.error(err);
-                        });
-                } else {
-                  client.sendText(receive.getFromMid(), receive.getText());
-                }
-
-            } else if(receive.isImage()) {
-                client.sendText(receive.getFromMid(), 'Thanks for the image!');
-            } else if(receive.isVideo()){
-                client.sendText(receive.getFromMid(), 'Thanks for the video!');
-            } else if(receive.isAudio()) {
-                client.sendText(receive.getFromMid(), 'Thanks for the audio!');
-            }else if(receive.isLocation()){
-                client.sendLocation(
-                    receive.getFromMid(),
-                    receive.getText() + receive.getAddress(),
-                    receive.getLatitude(),
-                    receive.getLongitude()
-                );
-            } else if(receive.isSticker()) {
-                // This only works if the BOT account have the same sticker too
-                client.sendSticker(
-                    receive.getFromMid(),
-                    receive.getStkId(),
-                    receive.getStkPkgId(),
-                    receive.getStkVer()
-                );
-            }else if(receive.isContact()){
-                client.sendText(receive.getFromMid(), 'Thanks for the contact');
-            }else{
-                console.error('found unknown message type');
-            }
-        } else if(receive.isOperation()) {
-            console.log('found operation');
-        } else {
-
-            console.error('invalid receive type');
-
-        }
-
-    });
-
-    res.send(200);
-});
-
-app.listen(app.get('port'), function () {
-  console.log('Listening on port ' + app.get('port'));
-});
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!')
+})
--- testIndex.js
+++ testIndex.js
@@ -1,45 +1,88 @@
 'use strict'
-const line = require('node-line-bot-api')
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 
-// need raw buffer for signature validation
-app.use(bodyParser.json({
-  verify (req, res, buf) {
-    req.rawBody = buf
-  }
-}))
+app.set('port', (process.env.PORT || 5000))
 
-// init with auth
-line.init({
-  accessToken: 'ToDFVvCpNFbfjtxzR2CDLNTt3aSWAhLN2/9jf03d0VGJSqh4DMSJd+fRm4ZH+TfNmzbWt6VCAosZqxsTmvmbIFLh7nQPLztM7/YIIryklIwG65ds9X11voXd8uPXqjabkrgCZYXnzo3dJNwJQwopIQdB04t89/1O/w1cDnyilFU=',
-  // (Optional) for webhook signature validation
-  channelSecret: '239ce8176ae2ca2edc3939bcf26241ec'
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
 })
 
-app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
-  // get content from request body
-  const promises = req.body.events.map(event => {
-    // reply message
-    console.log(event.replyToken)
-    return line.client
-      .replyMessage({
-        replyToken: event.replyToken,
-        messages: [
-          {
-            type: 'text',
-            text: event.message.text
-          }
-        ]
-      })
-  })
-
-  Promise
-    .all(promises)
-    .then(() => res.sendStatus(200))
+// Process Messages
+app.post('/', function (req, res) {
+    let messaging_events = req.body.events
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.events[i]
+        let sender = event.source.userId
+        let replyToken =  event.replyToken
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            replyLineMessage(replyToken, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+function replyLineMessage(replyToken, text) {
+	console.log('text: ', text)
+    request({
+        url: 'https://api.line.me/v2/bot/message/push',
+        Authorization: {Bearer: cT},
+        method: 'POST',
+        body: `{
+            replyToken: ${replyToken},
+            messages: [
+            	{
+	            	"type":"text",
+	            	"text":${text}
+	        	}
+	        ]
+        }`
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+function sendLineMessage(sender, text) {
+	console.log('text: ', text)
+
+    request({
+        url: 'https://api.line.me/v2/bot/message/push',
+        Authorization: {Bearer: cT},
+        method: 'POST',
+        body: {
+            to: {id: sender},
+            messages: [
+            	{
+	            	"type":"text",
+	            	"text":text
+	        	}
+	        ]
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+const cId = '1501651709'
+const cS = '239ce8176ae2ca2edc3939bcf26241ec'
+const cT = 'ToDFVvCpNFbfjtxzR2CDLNTt3aSWAhLN2/9jf03d0VGJSqh4DMSJd+fRm4ZH+TfNmzbWt6VCAosZqxsTmvmbIFLh7nQPLztM7/YIIryklIwG65ds9X11voXd8uPXqjabkrgCZYXnzo3dJNwJQwopIQdB04t89/1O/w1cDnyilFU='
